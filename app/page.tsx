625a434f090fc50795561e3195c24f826099022e
Note <it.spectre.ru@gmail.com>
2024-07-28 22:47:49 +0300

    add Title on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Container, Title } from "@/components/shared";
 
 export default function Home() {
-  return (
-    <h1>
-      Главная
-    </h1>
-  );
+  return <>
+    <Container className="mt-10">
+      <Title text="Все пиццы" size="lg" className="font-extrabold" />
+    </Container>
+  </>;
 }
