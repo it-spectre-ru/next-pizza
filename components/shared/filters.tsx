9aabde4aefeefd67a80cc8717e55fd4b702f9a41
Note <it.spectre.ru@gmail.com>
2024-07-29 19:35:59 +0300

    add input

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils';
 import React from 'react';
 import { Title } from './title';
 import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui';
 
 interface Props {
   className?: string;
@@ -15,6 +16,13 @@ export const Filters: React.FC<Props> = ({ className }) => {
       <div className='flex flex-col gap-4'>
         <FilterCheckbox text="Можно собирать" value="1" />
         <FilterCheckbox text="Новинки" value="2" />
+      </div>
+
+      <div className='mt-5 border-y-neutral-100 py-6 pb-7'></div>
+      <p className='font-bold mb-3'>Цена от и до:</p>
+      <div className='flex gap-3 mb-5'>
+        <Input type="number" placeholder='0' min={0} max={1000} defaultValue={0} />
+        <Input type="number" min={100} max={3000} placeholder='300000' />
       </div>
     </div>
   );
