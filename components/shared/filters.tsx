1c224aa473238d195287ccdf4e6e41847fefb1b1
Note <it.spectre.ru@gmail.com>
2024-07-30 13:20:07 +0300

    show / hidden list filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -50,6 +50,12 @@ export const Filters: React.FC<Props> = ({ className }) => {
           { text: 'Солёные огурчики', value: '4' },
           { text: 'Красный лук', value: '5' },
           { text: 'Томаты', value: '6' },
+          { text: 'Сырный соус', value: '1' },
+          { text: 'Моццарелла', value: '2' },
+          { text: 'Чеснок', value: '3' },
+          { text: 'Солёные огурчики', value: '4' },
+          { text: 'Красный лук', value: '5' },
+          { text: 'Томаты', value: '6' },
         ]}
       />
     </div>
