762ae264d8be890dbca2512620eb8c54891748ee
Note <it.spectre.ru@gmail.com>
2024-08-06 14:09:03 +0300

    get data from PostgreSQL

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,10 @@
+import { prisma } from "@/prisma/prisma-client";
 import { NextResponse } from "next/server";
 
 
 export async function GET() {
-  const users = ['user1', 'user2', 'user3'];
+  // SELECT * FROM users WHERE email = 'emasd'
+  const users = await prisma.user.findMany();
 
   return NextResponse.json(users);
 }
