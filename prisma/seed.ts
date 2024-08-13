5479a250017699e04c652663cf1859cef04d8584
Note <it.spectre.ru@gmail.com>
2024-08-13 15:49:16 +0300

    seed category

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { categories } from './constants';
 import { prisma } from './prisma-client';
 import { hashSync } from 'bcrypt';
 
@@ -21,7 +22,12 @@ async function up() {
       },
     ]
   })
+
+  await prisma.category.createMany({
+    data: categories,
+  })
 }
+
 
 async function down() {
   await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
